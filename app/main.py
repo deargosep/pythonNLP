f73ff62fb122c910ee9a789954408f3c9acf21f8
deargo <mchbtfl@gmail.com>
2021-10-02 07:05:45 +0600

    kinda optimised main py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from flask import request
 app = flask.Flask(__name__)
 
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 @app.route('/', methods=['GET'])
 def home():
@@ -13,13 +15,9 @@ def home():
 def talk():
     if request.form.__getitem__('text'):
         # initialize tokenizer and model from pretrained GPT2 model
-        tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-        model = GPT2LMHeadModel.from_pretrained('gpt2')
         inputs = tokenizer.encode(request.form.__getitem__('text'), return_tensors='pt')
-        outputs = model.generate(inputs, max_length=200, do_sample=True)
+        outputs = model.generate(inputs, max_length=200, do_sample=False)
         text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        print(text)
         return text
     else: 
-        print('no request data?')
         return '500'
